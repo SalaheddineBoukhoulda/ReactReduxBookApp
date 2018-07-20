60c6fa22730b61a7c5bbc7c54e8fcd19aefde701
RudeGames <fs_boukhoulda@esi.dz>
2018-07-20 22:23:32 +0100

    Added books reducer

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title : 'Rich dad poor dad'},
+        {title : 'Think and grow rich'},
+        {title : 'How to win friends and influence people'},
+        {title : 'Persuation'}
+    ];
+}
