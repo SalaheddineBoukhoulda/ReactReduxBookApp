e1bd630aab91d4aeaaaf1bcd8f95732325958ec4
RudeGames <fs_boukhoulda@esi.dz>
2018-07-23 14:21:10 +0100

    Added book details

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import BooksList from '../containers/book_list';
+import BookDetails from '../containers/bookdetails';
 export default class App extends Component {
   render() {
     return (
-      <BooksList />
+      <div>
+        <BooksList />
+        <BookDetails />
+      </div>
     );
   }
 }
